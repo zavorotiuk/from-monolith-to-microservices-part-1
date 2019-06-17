69358d709ac019f7cabb5a721fe3b0865366a61b
zavorotiuk <v.zavorotyuk@gmail.com>
2019-06-17 18:55:58 +0300

    Add logic fot creating order

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,6 +1,27 @@
+import Inventory from '../models/inventory.model';
+import Order from '../models/order.model';
+import { Types } from 'mongoose';
+import { verify } from 'jsonwebtoken';
+import config from '../core/config';
+
 export async function createOrder(ctx): Promise<void> {
     try {
-        ctx.body = {};
+        const inventory = await Inventory.findOne({_id: ctx.request.body.inventoryId}).exec();
+
+        if (!inventory) {
+            ctx.throw(404, 'Inventory not found');
+        }
+
+        const decodedJwt = verify(ctx.request.headers.authorization, config.jwt.secret);
+
+        const order = await new Order({
+            _id: new Types.ObjectId(),
+            inventoryId: inventory._id,
+            quantity: ctx.request.body.quantity,
+            userId: decodedJwt['userId']
+        }).save();
+
+        ctx.body = order;
     } catch (err) {
         ctx.app.emit('error', err, ctx);
     }
@@ -8,7 +29,7 @@ export async function createOrder(ctx): Promise<void> {
 
 export async function getOrderDetails(ctx): Promise<void> {
     try {
-        ctx.body = {};
+        ctx.body = await Order.findOne({_id: ctx.params.id}).exec();
     } catch (err) {
         ctx.app.emit('error', err, ctx);
     }
