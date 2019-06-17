06780dd2c8970f5b2bb02f9ee8da763ec6ad7be9
zavorotiuk <v.zavorotyuk@gmail.com>
2019-06-17 14:14:16 +0300

    Remove services and move logic to controllers

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,18 +1,52 @@
-import { createUser, getToken } from '../services/users.service';
+import { compare, hash } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { Types } from 'mongoose';
+import User from '../models/user.model';
+import config from '../core/config';
+
 
 export async function signin(ctx): Promise<void> {
     try {
-        const token = await getToken(ctx.request.body);
-        ctx.body = token;
+        const users = await User.find({ email: ctx.request.body.email }).exec();
+        if (users.length === 0) {
+            ctx.throw(404, 'User does not exist');
+        }
+
+        const passwordsMatch = await compare( ctx.request.body.password, users[0].password);
+
+        if (passwordsMatch) {
+            const token = sign({
+                email: users[0].email,
+                userId: users[0]._id
+            },
+            config.jwt.secret, {
+                expiresIn: '1h'
+            });
+
+            ctx.body = token;
+        }
     } catch (err) {
         ctx.app.emit('error', err, ctx);
     }
 }
 
 export async function signup(ctx): Promise<void> {
-    try { 
-        const user = await createUser(ctx.request.body);
-        ctx.body = user;
+    try {
+        const users = await User.find({ email: ctx.request.body.email}).exec();
+
+        if (users.length >= 1) {
+            ctx.throw(403, 'Email is allready used');
+        } else {
+            const passwordHash = await hash(ctx.request.body.password, 10);
+
+            ctx.body = new User({
+                _id: new Types.ObjectId(),
+                email: ctx.request.body.email,
+                firstName: ctx.request.body.firstName,
+                lastName: ctx.request.body.lastName,
+                password: passwordHash
+            }).save();
+        }
     } catch (err) {
         ctx.app.emit('error', err, ctx);
     }
@@ -20,7 +54,7 @@ export async function signup(ctx): Promise<void> {
 
 export async function getUserDetails(ctx): Promise<void> {
     try {
-        ctx.body = {};
+        ctx.body = await User.findById(ctx.request.params.id);
     } catch (err) {
         ctx.app.emit('error', err, ctx);
     }
