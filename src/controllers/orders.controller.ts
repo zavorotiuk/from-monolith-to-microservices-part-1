efb6f9be369d4cc47995f17e46fb56d277e63028
zavorotiuk <v.zavorotyuk@gmail.com>
2019-06-17 22:21:35 +0300

    Add authorization middleware, update routers to use middleware, update orders controller to use ctx.state.user instead of decoding jwt inside controller

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,8 +1,6 @@
 import Inventory from '../models/inventory.model';
 import Order from '../models/order.model';
 import { Types } from 'mongoose';
-import { verify } from 'jsonwebtoken';
-import config from '../core/config';
 
 export async function createOrder(ctx): Promise<void> {
     try {
@@ -12,13 +10,11 @@ export async function createOrder(ctx): Promise<void> {
             ctx.throw(404, 'Inventory not found');
         }
 
-        const decodedJwt = verify(ctx.request.headers.authorization, config.jwt.secret);
-
         const order = await new Order({
             _id: new Types.ObjectId(),
             inventoryId: inventory._id,
             quantity: ctx.request.body.quantity,
-            userId: decodedJwt['userId']
+            userId: ctx.state.user
         }).save();
 
         ctx.body = order;
